c4819ae49f604631a883341f325965a85e7afd51
dyfloveslife <786616590@qq.com>
2019-11-19 19:56:37 +0800

    update file

--- src/JianzhiOfferSolution/_27_MirrorOfBinaryTree/Solution.java
+++ src/JianzhiOfferSolution/_27_MirrorOfBinaryTree/Solution.java
@@ -2,18 +2,17 @@ package JianzhiOfferSolution._27_MirrorOfBinaryTree;
 
 /**
  * 求二叉树的镜像
+ * 思路：
+ * 前序遍历树中的每个节点，如果遍历到的节点有子节点，则交换两个子节点。
+ * 当交换完所有非叶节点的左右子节点的时候，就可以得到二叉树的镜像。
  */
 class TreeNode {
 	int val = 0;
-	TreeNode left;
-	TreeNode right;
-	TreeNode parent;
+	TreeNode left = null;
+	TreeNode right = null;
 
 	TreeNode(int val) {
 		this.val = val;
-		this.left = null;
-		this.right = null;
-		this.parent = null;
 	}
 }
 
