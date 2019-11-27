ac5ea45be552f5c04485d887fb43c024e4d5dd15
dyfloveslife <786616590@qq.com>
2019-11-27 15:56:11 +0800

    add files

---
+++ src/SwordToOfferSolution/_41_02_FirstAppearingOnce/Solution.java
@@ -0,0 +1,41 @@
+package SwordToOfferSolution._41_02_FirstAppearingOnce;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 字符流中第一个不重复的字符
+ * <p>
+ * 题目描述:
+ * 请实现一个函数用来找出字符流中第一个只出现一次的字符。
+ * 例如，当从字符流中只读出前两个字符 “go” 时，第一个只出现一次的字符是 "g"；
+ * 当从该字符流中读出前六个字符 “google” 时，第一个只出现一次的字符是 "l"。
+ * 如果当前字符流没有存在出现一次的字符，返回 # 字符。
+ *
+ * 思路：
+ * 1. 构造一个哈希数组，每次 insert 的时候统计当前字符出现的次数，并将该字符加入到队列中；
+ * 2. 只要队头字符的数量大于 1，则说明该字符是重复的，即让其出队；
+ * 3. 由于重复的字符都出队了，所以最后返回队头元素就是第一个不重复出现的字符。
+ */
+public class Solution {
+
+    private int[] count = new int[256];
+    private Queue<Character> queue = new LinkedList<>();
+
+    // 从字符流中插入一个字符
+    public void insert(char ch) {
+        // ch 就是对应的 ASCII 值
+        // 统计每个 ch 的数量
+        count[ch]++;
+        queue.offer(ch); // 入队
+        while (!queue.isEmpty() && count[queue.peek()] > 1)
+            queue.poll(); // 出队
+
+    }
+
+    // 返回当前字符流中第一个不重复出现的字符
+    public char firstAppearingOnce() {
+        return queue.isEmpty() ? '#' : queue.peek();
+
+    }
+}
