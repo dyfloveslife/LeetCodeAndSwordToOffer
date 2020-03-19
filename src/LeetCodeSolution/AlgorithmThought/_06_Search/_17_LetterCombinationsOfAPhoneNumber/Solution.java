b47e9ac2a5dba8a63e04a9796df98615d1fd733a
dyfloveslife <786616590@qq.com>
2020-03-19 18:45:50 +0800

    add LeetCode solution

---
+++ src/LeetCodeSolution/AlgorithmThought/_06_Search/_17_LetterCombinationsOfAPhoneNumber/Solution.java
@@ -0,0 +1,58 @@
+package LeetCodeSolution.AlgorithmThought._06_Search._17_LetterCombinationsOfAPhoneNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * 电话号码的组合
+ *
+ * 题目描述：
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ * 给出数字到字母的映射如下（与电话按键相同）。
+ * 注意 1 不对应任何字母。
+ *
+ * 思路：
+ * 0. https://www.youtube.com/watch?v=iFP-xQhg6IU
+ * 1. 回溯：
+ * 2. 输入的是一个字符串数字，需要用给定的数字找到对应的字符映射关系；
+ * 3. 例如给定 "27"，则 2 对应的字符是 "abc"，7 对应的字符是 "pqrs"；
+ *    3.1) 首先拿出 2 中的 'a'，然后用 'a' 去与 'p' 组合，生成 "ap"；
+ *    3.2) 然后还得再回到 2 中的 'a'，再用 'a' 去与 'q' 组合，生成 "aq"；
+ *    3.3) 一直组合，一直回溯，直至结束。
+ *
+ */
+public class Solution {
+
+    // 索引 0 和 1 不对应任何字符串
+    String[] keyboard = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        // 0 表示从 digits 的第 0 个位置开始进行组合
+        dfs(digits, 0, sb, res);
+        return res;
+    }
+
+    private void dfs(String digits, int index, StringBuilder sb, List<String> res) {
+        // 如果已经来到了 digits 的末尾，则说明对于给定的字符串数字，已经遍历完了
+        if (index == digits.length()) {
+            res.add(sb.toString());
+            return;
+        }
+        // 例如给定的 digits 是 "27"，则 letters 就表示字符串 "abc"
+        String letters = keyboard[digits.charAt(index) - '0'];
+        // 用 for 循环依次拿出 abc 中的每个字符，即 'a'、'b'、'c'
+        for (int i = 0; i < letters.length(); i++) {
+            char c = letters.charAt(i);
+            // 先将字符 'a' 添加进行 sb
+            sb.append(c);
+            // 然后再来到字符串数字 7 中对应的 "pqrs"，分别与 'a' 进行组合
+            // index + 1 指向的是字符串数字 7
+            dfs(digits, index + 1, sb, res);
+            // 由于子过程给我的 sb 返回了 ap，所以需要将 p 删除
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
---
+++ src/LeetCodeSolution/AlgorithmThought/_06_Search/_417_PacificAtlanticWaterFlow/Solution.java
@@ -0,0 +1,98 @@
+package LeetCodeSolution.AlgorithmThought._06_Search._417_PacificAtlanticWaterFlow;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * 太平洋大西洋水流问题
+ *
+ * 题目描述：
+ * 1. 给定一个 m x n 的非负整数矩阵来表示一片大陆上各个单元格的高度。“太平洋”处于大陆的左边界和上边界，而“大西洋”处于大陆的右边界和下边界。
+ * 2. 规定水流只能按照上、下、左、右四个方向流动，且只能从高到低或者在同等高度上流动。
+ * 3. 请找出那些水流既可以流动到“太平洋”，又能流动到“大西洋”的陆地单元的坐标。
+ * 4. 示例：
+ *           太平洋 ~   ~   ~   ~   ~
+ *             ~  1   2   2   3  (5) *
+ *             ~  3   2   3  (4) (4) *
+ *             ~  2   4  (5)  3   1  *
+ *             ~ (6) (7)  1   4   5  *
+ *             ~ (5)  1   1   2   4  *
+ *                *   *   *   *   * 大西洋
+ *
+ * 5. 输出：[[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (上图中带括号的单元).
+ *
+ * 思路：
+ * 0. 使用 DFS；
+ * 1. 建立两个矩阵 Pacific（太平洋）和 Atlantic（大西洋），
+ *    当 Atlantic[i][j] 和 Pacific[i][j] 同时为 true 时，表示当前位置 [i][j] 可以同时到达 Atlantic 和 Pacific；
+ * 2. 从四个边界出发，如果能够到达的话，则说明当前位置的水可以流向对应的海洋。
+ *
+ */
+public class Solution {
+
+    public static List<List<Integer>> pacificAtlantic(int[][] matrix) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return res;
+        }
+
+        int m = matrix.length, n = matrix[0].length;
+
+        boolean[][] Pacific = new boolean[m][n];
+        boolean[][] Atlantic = new boolean[m][n];
+
+        // 注意每个边界都对应不同的海洋，需要分别处理
+        for (int i = 0; i < n; i++) {
+            // 上边界
+            dfs(matrix, 0, i, Pacific, matrix[0][i]);
+            // 下边界
+            dfs(matrix, m - 1, i, Atlantic, matrix[m - 1][i]);
+        }
+
+        for (int j = 0; j < m; j++) {
+            // 左边界
+            dfs(matrix, j, 0, Pacific, matrix[j][0]);
+            // 右边界
+            dfs(matrix, j, n - 1, Atlantic, matrix[j][n - 1]);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // 当前的位置 [i][j] 可以到达大西洋和太平洋，
+                // 将该位置添加到结果集中
+                if (Pacific[i][j] && Atlantic[i][j]) {
+                    res.add(Arrays.asList(i, j));
+                }
+            }
+        }
+        return res;
+    }
+
+    // pre_h 表示之前的高度
+    private static void dfs(int[][] matrix, int i, int j, boolean[][] visited, int pre_h) {
+        // 边界检查
+        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length
+                || visited[i][j] || matrix[i][j] < pre_h) {
+            return;
+        }
+
+        visited[i][j] = true;
+        dfs(matrix, i + 1, j, visited, matrix[i][j]);
+        dfs(matrix, i - 1, j, visited, matrix[i][j]);
+        dfs(matrix, i, j + 1, visited, matrix[i][j]);
+        dfs(matrix, i, j - 1, visited, matrix[i][j]);
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 2, 3, 5},
+                {3, 2, 3, 4, 4},
+                {2, 4, 5, 3, 1},
+                {6, 7, 1, 4, 5},
+                {5, 1, 1, 2, 4}
+        };
+
+        System.out.println(pacificAtlantic(arr));
+    }
+}
