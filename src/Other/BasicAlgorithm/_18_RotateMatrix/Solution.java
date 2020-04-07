215cdcf49b64c65ca807ca743473bcbdf48244cd
dyfloveslife <786616590@qq.com>
2020-04-07 10:33:19 +0800

    update code

--- src/Other/BasicAlgorithm/_18_RotateMatrix/Solution.java
+++ src/Other/BasicAlgorithm/_18_RotateMatrix/Solution.java
@@ -1,60 +1,68 @@
 package Other.BasicAlgorithm._18_RotateMatrix;
 
+import java.util.Arrays;
+
 /*
- * 将正方形矩阵顺时针旋转 90°
- * 不申请额外空间，即找到哪四个点是一组，然后进行交换。
+ * 旋转矩阵
+ *
+ * 题目描述：
+ * 将正方形矩阵顺时针旋转 90°。
+ *
+ * 思路：
+ * 1. 不申请额外空间，即找到哪四个点是一组，然后进行交换；
+ * 2. 交换的时候先将左上角的点保存下来，然后分为如下几步：
+ *    将左上角的点指向左下角的点，
+ *    将左下角的点指向右下角的点，
+ *    将右下角的点指向右上角的点，
+ *    将右上角的点指向左上角的点。
+ * 3. 交换完 4 个点后，然后 i++，来到下一个位置继续交换下一轮的 4 个点。
  */
 public class Solution {
-    public static void rotate(int[][] matrix) {
-        // tR 左上角的行
-        // tC 左上角的列
-        // dR 右下角的行
-        // dC 右下角的列
-        int tR = 0;
-        int tC = 0;
-        int dR = matrix.length - 1;
-        int dC = matrix[0].length - 1;
+    public void rotate(int[][] matrix) {
+        // 左上角的点 (lRow, lCol)
+        // 右下角的点 (rRow, rCol)
+        int lRow = 0;
+        int lCol = 0;
+        int rRow = matrix.length - 1;
+        int rCol = matrix[0].length - 1;
 
-        while (tR < dR) {
-            rotateEdge(matrix, tR++, tC++, dR--, dC--);
+        while (lRow < rRow) {
+            process(matrix, lRow++, lCol++, rRow--, rCol--);
         }
     }
 
-    private static void rotateEdge(int[][] matrix, int tR, int tC, int dR, int dC) {
-        // 元素 1 对应四个点，分别交换
-        // 元素 2 对应四个点，分别交换
-        // 元素 3 对应四个点，分别交换
-        int times = dC - tC;
+    public void process(int[][] matrix, int lRow, int lCol, int rRow, int rCol) {
+        int times = rCol - lCol;
         int temp = 0;
-        for (int i = 0; i != times; i++) {
+        for (int i = 0; i < times; i++) {
+            // i++ 就是来到下一个位置再继续进行 4 个点的交换
             // 逆时针交换
             // temp =  (0, 0)
             // (0, 0) = (3, 0)
             // (3, 0) = (3, 3)
             // (3, 3) = (0, 3)
             // (0, 0) = temp
-            temp = matrix[tR][tC + i];
-            matrix[tR][tC + i] = matrix[dR - i][tC];
-            matrix[dR - i][tC] = matrix[dR][dC - i];
-            matrix[dR][dC - i] = matrix[tR + i][dC];
-            matrix[tR + i][dC] = temp;
+            temp = matrix[lRow][lCol + i];
+            matrix[lRow][lCol + i] = matrix[rRow - i][lCol];
+            matrix[rRow - i][lCol] = matrix[rRow][rCol - i];
+            matrix[rRow][rCol - i] = matrix[lRow + i][rCol];
+            matrix[lRow + i][rCol] = temp;
         }
-    }
-
-    public static void printCotateEdge(int[][] matrix) {
-        for (int i = 0; i != matrix.length; i++) {
-            for (int j = 0; j != matrix[0].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-        }
-        System.out.println();
     }
 
     public static void main(String[] args) {
-        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        printCotateEdge(matrix);
-        rotate(matrix);
-        System.out.println("-----------");
-        printCotateEdge(matrix);
+        int[][] matrix = {
+                {1,   2,  3,  4},
+                {5,   6,  7,  8},
+                {9,  10, 11, 12},
+                {13, 14, 15, 16}
+        };
+
+        Solution solution = new Solution();
+        solution.rotate(matrix);
+
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
