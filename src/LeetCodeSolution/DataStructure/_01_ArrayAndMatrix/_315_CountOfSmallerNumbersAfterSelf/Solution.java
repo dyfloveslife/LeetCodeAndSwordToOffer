864537ece70f0810dc5293d30ce85ccdb09f8289
dyfloveslife <786616590@qq.com>
2020-07-11 15:58:50 +0800

    other PC push file

---
+++ src/LeetCodeSolution/DataStructure/_01_ArrayAndMatrix/_315_CountOfSmallerNumbersAfterSelf/Solution.java
@@ -0,0 +1,19 @@
+package LeetCodeSolution.DataStructure._01_ArrayAndMatrix._315_CountOfSmallerNumbersAfterSelf;
+
+import java.util.List;
+
+/*
+ * 计算右侧小于当前元素的个数
+ *
+ * 题目描述：
+ * 给定一个整数数组 nums，按要求返回一个新数组 counts。
+ * 数组 counts 有该性质：counts[i] 的值是 nums[i] 右侧小于 nums[i] 的元素的数量。
+ *
+ * 思路：
+ * 1.
+ */
+public class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        return null;
+    }
+}
