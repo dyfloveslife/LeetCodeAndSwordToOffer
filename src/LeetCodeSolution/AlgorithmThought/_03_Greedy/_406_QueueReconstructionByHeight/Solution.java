5dbaea5cfd5e4f15c554f9275d85379e9d71e962
dyfloveslife <786616590@qq.com>
2020-11-16 16:37:56 +0800

    update code

--- src/LeetCodeSolution/AlgorithmThought/_03_Greedy/_406_QueueReconstructionByHeight/Solution.java
+++ src/LeetCodeSolution/AlgorithmThought/_03_Greedy/_406_QueueReconstructionByHeight/Solution.java
@@ -1,6 +1,9 @@
 package LeetCodeSolution.AlgorithmThought._03_Greedy._406_QueueReconstructionByHeight;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 /*
  * 根据身高重建队列
@@ -14,11 +17,11 @@ import java.util.*;
  * 1. 先按照 h 进行降序，h 相等的话则按照 k 进行升序排序；
  * 2. 假如队列中所有的人的 (h, k) 中 h 都相等，只有 k 不同的话，则每个人在队列的索引 index==k；
  * 3. 假设候选队列为 A，已排好的队列为 B，则从 A 中选一个身高最高的人 x，插入到 B 中；
- * 4. 由于在已排好的队列 B 中的每个人都比 x 要高，因此 x 插入的位置就是看 x 前面有多少个人进行了；
+ * 4. 由于在已排好的队列 B 中的每个人都比 x 要高，因此 x 插入的位置就是看 x 前面有多少个人就行了；
  * 5. 比如 x 的前面有 k=5 个人，则 x 就直接插入到 B 中第 5 个位置即可。
  */
 public class Solution {
-    public static int[][] reconstructQueue(int[][] people) {
+    public int[][] reconstructQueue(int[][] people) {
         if (people == null || people.length == 0) {
             return new int[0][0];
         }
@@ -31,16 +34,18 @@ public class Solution {
             }
         });
 
-        List<int[]> queue = new LinkedList<>();
+        List<int[]> ans = new ArrayList<>();
         for (int[] p : people) {
-            // add(index, element)
-            queue.add(p[1], p);
+            // equals add(index, element)
+            ans.add(p[1], p);
         }
-        return queue.toArray(new int[people.length][2]);
+        return ans.toArray(new int[people.length][2]);
     }
 
     public static void main(String[] args) {
+        Solution solution = new Solution();
         int[][] arr = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
-        System.out.println(Arrays.deepToString(reconstructQueue(arr)));
+
+        System.out.println(Arrays.deepToString(solution.reconstructQueue(arr)));
     }
 }
