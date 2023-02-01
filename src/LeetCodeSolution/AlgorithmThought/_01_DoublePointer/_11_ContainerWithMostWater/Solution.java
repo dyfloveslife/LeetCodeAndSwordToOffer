9db98c9a73fc7815eec2ebe5165ca184b9198b6d
dyfloveslife <786616590@qq.com>
2023-02-01 17:45:54 +0800

    feat: update the code for question 11.

--- src/LeetCodeSolution/AlgorithmThought/_01_DoublePointer/_11_ContainerWithMostWater/Solution.java
+++ src/LeetCodeSolution/AlgorithmThought/_01_DoublePointer/_11_ContainerWithMostWater/Solution.java
@@ -4,15 +4,16 @@ package LeetCodeSolution.AlgorithmThought._01_DoublePointer._11_ContainerWithMos
  * 盛最多水的容器
  *
  * 题目描述：
- * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai)。
- * 在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。
+ * 给定一个长度为 n 的整数数组 height。
+ * 有 n 条垂线，第 i 条线的两个端点分别是 (i, 0) 和 (i, height[i])。
  * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
  *
  * 说明：你不能倾斜容器，且 n 的值至少为 2。
  *
  * 思路：
  * 1. 使用双指针，从数组两端不断地向中间靠拢；
- * 2. 每次比较两个指针所指的元素大小（也就是比较高度），水的多少取决于短板的长度再乘以两个指针之间的距离。
+ * 2. 每次比较两个指针所指的元素大小（也就是比较高度），水的多少取决于短板的长度再乘以两个指针之间的距离；
+ * 3. 即维护一个最终的结果 res，每次计算水的容量时更新 res，遍历完整个数组后，最终的 res 就是答案。
  */
 public class Solution {
     public int maxArea(int[] height) {
@@ -21,8 +22,7 @@ public class Solution {
         }
 
         int res = 0;
-        int left = 0;
-        int right = height.length - 1;
+        int left = 0, right = height.length - 1;
 
         while (left < right) {
             if (height[left] < height[right]) {
@@ -33,6 +33,7 @@ public class Solution {
                 right--;
             }
         }
+
         return res;
     }
 
