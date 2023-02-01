b7dd0aa70a17149323e15523d6e98a84383b40f3
dyfloveslife <786616590@qq.com>
2023-02-01 17:06:49 +0800

    feat: update the code for question 4..

--- src/LeetCodeSolution/DataStructure/_01_ArrayAndMatrix/_4_MedianOfTwoSortedArrays/Solution.java
+++ src/LeetCodeSolution/DataStructure/_01_ArrayAndMatrix/_4_MedianOfTwoSortedArrays/Solution.java
@@ -4,9 +4,9 @@ package LeetCodeSolution.DataStructure._01_ArrayAndMatrix._4_MedianOfTwoSortedAr
  * 寻找两个正序数组的中位数
  *
  * 题目描述：
- * 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
- * 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
- * 你可以假设 nums1 和 nums2 不会同时为空。
+ * 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
+ * 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * 你可以假设 nums1 和 nums2 不会同时为空。
  *
  * 思路一：使用额外的辅助空间
  * 1. 常规的做法是：先将两个数组合并，然后再排序，根据奇偶进行判断；
@@ -14,71 +14,80 @@ package LeetCodeSolution.DataStructure._01_ArrayAndMatrix._4_MedianOfTwoSortedAr
  * 3. 时间复杂度为 O(m+n)。
  *
  * 思路二：二分
- * 1. 详细解释：https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/he-bing-yi-hou-zhao-gui-bing-guo-cheng-zhong-zhao-/
- * 2. https://www.youtube.com/watch?v=ScCg9v921ns
- * 3. 可以使用二分法，通过二分法找到“边界线”：
- *    对于偶数来说，“边界线”的两侧分别为 left_max 和 right_min，然后直接取平均值就好了；
- *    对于奇数来说，取“边界线”的左侧作为中位数。
- * 4. 在有序数组中找中位数，其实就是找“边界线”，边界线找对了，中位数也就知道了；
- * 5. 也就是说，在较短的那个数组中找到分割线，使得 L1 ≤ R1，L2 ≤ R2；
- * 6. 为什么选择“较短的那个数组”，因为对于较短的数组来说，我去找它的分割线的时候，用时也是很短的；
+ * 详细解释：
+ *   https://leetcode.com/problems/median-of-two-sorted-arrays/solutions/2471/very-concise-o-log-min-m-n-iterative-solution-with-detailed-explanation/
+ *   https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/he-bing-yi-hou-zhao-gui-bing-guo-cheng-zhong-zhao-/
+ *   https://www.youtube.com/watch?v=ScCg9v921ns
  */
 public class Solution {
     // 思路一
     public double findMedianSortedArrays1(int[] nums1, int[] nums2) {
-        int[] mergeResult = merge(nums1, nums2);
-        int n = mergeResult.length;
-        return (n & 1) == 1 ? (mergeResult[n / 2]) : (mergeResult[n / 2 - 1] + mergeResult[n / 2]) / 2.0;
-    }
+        if (nums1 == null || nums2 == null) {
+            return 0;
+        }
 
-    private int[] merge(int[] nums1, int[] nums2) {
-        int[] nums = new int[nums1.length + nums2.length];
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        while (i < nums1.length && j < nums2.length) {
-            nums[k++] = (nums1[i] < nums2[j]) ? nums1[i++] : nums2[j++];
+        int length1 = nums1.length, length2 = nums2.length;
+        int[] nums = new int[length1 + length2];
+
+        // idx、i、j 分别指向 nums、nums1、nums2 的起始位置
+        int idx = 0, p1 = 0, p2 = 0;
+        while (p1 < length1 && p2 < length2) {
+            nums[idx++] = nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
         }
-        while (i < nums1.length) {
-            nums[k++] = nums1[i++];
+        while (p1 < length1) {
+            nums[idx++] = nums1[p1++];
         }
-        while (j < nums2.length) {
-            nums[k++] = nums2[j++];
+        while (p2 < length2) {
+            nums[idx++] = nums2[p2++];
         }
-        return nums;
+
+        int length = nums.length;
+        return (length & 1) == 1 ? nums[length / 2] : (nums[length / 2 - 1] + nums[length / 2]) / 2.0;
     }
 
     // 思路二
     public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        if (len1 > len2) {
-            return findMedianSortedArrays2(nums2, nums1);
+        // 在短的数组上找分割线
+        if (nums1.length > nums2.length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
         }
-        // 如果 nums1 没有元素，则需要从 nums2 中找到中位数
-        if (len1 == 0) {
-            return (nums2[(len2 - 1) / 2] + nums2[len2 / 2]) / 2.0;
-        }
-        int len = len1 + len2;
-        int start = 0;
-        int end = len1;
-        int curA, curB;
-        while (start <= end) {
-            curA = (start + end) / 2;
-            curB = (len + 1) / 2 - curA;
-            double L1 = curA == 0 ? Integer.MIN_VALUE : nums1[curA - 1];
-            double L2 = curB == 0 ? Integer.MIN_VALUE : nums2[curB - 1];
-            double R1 = curA == len1 ? Integer.MAX_VALUE : nums1[curA];
-            double R2 = curB == len2 ? Integer.MAX_VALUE : nums2[curB];
-            if (L1 > R2) {
-                end = curA - 1;
-            } else if (L2 > R1) {
-                start = curA + 1;
+        // m < n
+        int m = nums1.length, n = nums2.length;
+
+        // 分割线左边的所有元素需要满足的个数为 m + (n - m + 1) / 2, 该表达式是是为了防止数组长度相加后溢出
+        int totalLeft = (m + n + 1) / 2;
+
+        // 在短数组 nums1 的区间 [0, m] 中查找恰当的分割线
+        // 使得 nums1[i - 1] <= nums2[j] && nums2[j - 1] <= nums1[i]
+        // 也就是需要满足 nums1 中分割线左侧的元素小于等于 nums2 中分割线右侧的元素
+        // 同时 nums2 中分割线左侧的元素也要小于等于 nums1 中分割线右侧的元素
+        int left = 0, right = m;
+        while (left < right) {
+            // i、j 指向分割线的位置
+            int i = left + (right - left + 1) / 2;
+            int j = totalLeft - i;
+            if (nums1[i - 1] > nums2[j]) {
+                // nums1 中分割线左侧的数多了, 因此分割线要左移, 下一轮的搜索区间为 [left, i - 1]
+                right = i - 1;
             } else {
-                return (len & 1) == 1 ? Math.max(L1, L2) : (Math.max(L1, L2) + Math.min(R1, R2)) / 2;
+                // 下一轮的搜索区间为 [i, right]
+                left = i;
             }
         }
-        return -1;
+
+        int i = left, j = totalLeft - i;
+        int nums1LeftMax = (i == 0) ? Integer.MIN_VALUE : nums1[i - 1];
+        int nums1RightMin = (i == m) ? Integer.MAX_VALUE : nums1[i];
+        int nums2LeftMax = (j == 0) ? Integer.MIN_VALUE : nums2[j - 1];
+        int nums2RightMin = (j == n) ? Integer.MAX_VALUE : nums2[j];
+
+        if (((m + n) & 1) == 1) {
+            return Math.max(nums1LeftMax, nums2LeftMax);
+        } else {
+            return (Math.max(nums1LeftMax, nums2LeftMax) + Math.min(nums1RightMin, nums2RightMin)) / 2.0;
+        }
     }
 
     public static void main(String[] args) {
