efab43e43e034f454418d5e8355d8140b12c8544
dyfloveslife <786616590@qq.com>
2023-02-07 10:11:33 +0800

    feat: update the code for question 41.

--- src/LeetCodeSolution/DataStructure/_01_ArrayAndMatrix/_41_FirstMissingPositive/Solution.java
+++ src/LeetCodeSolution/DataStructure/_01_ArrayAndMatrix/_41_FirstMissingPositive/Solution.java
@@ -2,108 +2,131 @@ package LeetCodeSolution.DataStructure._01_ArrayAndMatrix._41_FirstMissingPositi
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 /*
  * 缺失的第一个正整数
  *
  * 题目描述：
  * 给你一个未排序的整数数组，请你找出其中没有出现的最小的正整数。
- *
  * 你的算法的时间复杂度应为O(n)，并且只能使用常数级别的额外空间。
  *
- * 思路：
- * 1. 如果不考虑额外空间复杂度，那么可以从数字 1 开始，依次判断 2、3、4、... 是否在数组中；
- * 2. 如果的当前考虑的数不在数组中，那么我们就找到了这个缺失的最小正数。
- * 3. 然后就是二分查找的思路，先对数组排序，然后对于从 1 开始的每个元素，都去进行二分；
- * 4. 如果当前进行二分的 i 找不到的话，那么就返回这个 i 即可。
- * 5. 还有一种思路是：将 1 放在下标为 0 的位置，将 2 放在下标为 1 的位置；
- * 6. 整理完数组后，然后再遍历一遍数组，第一个遇到的元素值不等于下标的数，就是缺失的数。
+ * 思路一：HashSet
+ * 0. 首先将每个元素放到 Set 中，
+ * 1. 然后从数字 1 开始遍历，若数字 x 不在 Set 中，说明缺失的就是该数，直接返回即可（此时如果能直接返回的话，说明原始数据中不含有数字 1）；
+ * 2. 如果没有返回，则说明原始数字中含有 1，那么"其中没有出现的最小的正整数"就是数组长度加一。
+ *
+ * 思路二：排序 + 二分
+ * 1. 先对数组排序，然后对于从 1 开始的每个元素，都去进行二分；
+ * 2. 如果当前进行二分的 i 找不到的话，那么就返回这个 i 即可。
+ *
+ * 思路三：置换（将数组视为哈希表）
+ * 1. 使用两次循环，第一次循环的作用是：将元素为 i 的数映射到索引为 i - 1 的位置，即索引 0 存储数字 1，索引 1 存储数字 2，索引 2 存储数字 3；
+ * 2. 也就是说，对于每一个正整数，我们使用元素值作为索引去存储它在数组中对应的值；
+ * 3. 整理完数组后，然后再遍历一遍数组，第一个缺失的正数将是映射关系不对的那个数；
+ * 4. 此外，由于题目要求的限制（正整数），要找的数一定在 [1, n + 1] 里面，其中 n 为数组的长度；
+ * 5. 但其实最后一个元素 n + 1 不用找，因为在前面 n 个元素都找不到的情况下，直接返回 n + 1 即可。
  */
 public class Solution {
+    // 思路一
     public int firstMissingPositive(int[] nums) {
         if (nums == null) {
             return -1;
         }
-        // 注意此边界条件
-        if (nums.length == 0) {
+
+        int length = nums.length;
+        if (length == 0) {
             return 1;
         }
 
-        HashSet<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
         }
 
-        for (int i = 1; i <= nums.length; ++i) {
+        for (int i = 1; i <= length; ++i) {
             if (!set.contains(i)) {
                 return i;
             }
         }
         // 如果遍历完数组后，没有找到第一个最小的正整数，
         // 那么就说明整个数组是一个升序的、公差为 1 的等差数列，
-        // 所以，第一个最小的正整数因该就是数组的长度
-        return nums.length + 1;
+        // 所以，第一个最小的正整数应该就是数组的长度加一
+        return length + 1;
     }
 
+    // 思路二
     public int firstMissingPositive2(int[] nums) {
         if (nums == null) {
             return -1;
         }
-        if (nums.length == 0) {
+
+        int length = nums.length;
+        if (length == 0) {
             return 1;
         }
 
         Arrays.sort(nums);
-        for (int i = 1; i <= nums.length; ++i) {
+        for (int i = 1; i <= length; ++i) {
             // 等于 -1 说明这个数缺失了
             if (binarySearch(nums, i) == -1) {
                 return i;
             }
         }
-        return nums.length + 1;
+
+        return length + 1;
     }
 
     private int binarySearch(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length - 1;
-        while (l <= r) {
-            int mid = l + ((r - l) >> 1);
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + ((j - i) >> 1);
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
-                l = mid + 1;
+                i = mid + 1;
             } else {
-                r = mid - 1;
+                j = mid - 1;
             }
         }
+
         return -1;
     }
 
+    // 思路三
     public int firstMissingPositive3(int[] nums) {
         if (nums == null) {
             return -1;
         }
-        if (nums.length == 0) {
+
+        int length = nums.length;
+        if (length == 0) {
             return 1;
         }
 
-        for (int i = 0; i < nums.length; ++i) {
-            while (nums[i] > 0 && nums[i] < nums.length && nums[nums[i] - 1] != nums[i]) {
+        for (int i = 0; i < length; i++) {
+            // 将元素 i 映射到索引 i - 1 的位置
+            // 例如，元素 3 应该放在索引 2 的位置
+            while (nums[i] > 0 && nums[i] < length && nums[i] != nums[nums[i] - 1]) {
                 swap(nums, i, nums[i] - 1);
             }
         }
-        for (int i = 0; i < nums.length; ++i) {
+        // [1, -1, 3, 4]
+        //  0   1  2  3
+        for (int i = 0; i < length; i++) {
             if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
-        return nums.length + 1;
+
+        // 如果所有元素的摆放位置都正确，则直接返回数组长度加一
+        return length + 1;
     }
 
     private void swap(int[] nums, int i, int j) {
-        int t = nums[i];
+        int temp = nums[i];
         nums[i] = nums[j];
-        nums[j] = t;
+        nums[j] = temp;
     }
 
     public static void main(String[] args) {
@@ -111,22 +134,17 @@ public class Solution {
         int[] nums1 = {1, 2, 0};
         int[] nums2 = {3, 4, -1, 1};
         int[] nums3 = {7, 8, 9, 11, 12};
-        int[] nums4 = {};
 
-        System.out.println(solution.firstMissingPositive(nums1));
-        System.out.println(solution.firstMissingPositive(nums2));
-        System.out.println(solution.firstMissingPositive(nums3));
-        System.out.println(solution.firstMissingPositive(nums4));
+        System.out.println(solution.firstMissingPositive(nums1)); // 3
+        System.out.println(solution.firstMissingPositive(nums2)); // 2
+        System.out.println(solution.firstMissingPositive(nums3)); // 1
         System.out.println("--");
-        System.out.println(solution.firstMissingPositive2(nums1));
-        System.out.println(solution.firstMissingPositive2(nums2));
-        System.out.println(solution.firstMissingPositive2(nums3));
-        System.out.println(solution.firstMissingPositive2(nums4));
+        System.out.println(solution.firstMissingPositive2(nums1)); // 3
+        System.out.println(solution.firstMissingPositive2(nums2)); // 2
+        System.out.println(solution.firstMissingPositive2(nums3)); // 1
         System.out.println("--");
-        System.out.println(solution.firstMissingPositive3(nums1));
-        System.out.println(solution.firstMissingPositive3(nums2));
-        System.out.println(solution.firstMissingPositive3(nums3));
-        System.out.println(solution.firstMissingPositive3(nums4));
-
+        System.out.println(solution.firstMissingPositive3(nums1)); // 3
+        System.out.println(solution.firstMissingPositive3(nums2)); // 2
+        System.out.println(solution.firstMissingPositive3(nums3)); // 1
     }
 }
