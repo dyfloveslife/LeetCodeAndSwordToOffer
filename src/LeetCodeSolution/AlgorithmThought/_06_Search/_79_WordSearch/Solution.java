78228eca7743485ce5833ccb1627d181ac4601a3
dyfloveslife <786616590@qq.com>
2023-02-13 17:17:41 +0800

    feat: update the code for question 79.

--- src/LeetCodeSolution/AlgorithmThought/_06_Search/_79_WordSearch/Solution.java
+++ src/LeetCodeSolution/AlgorithmThought/_06_Search/_79_WordSearch/Solution.java
@@ -10,29 +10,34 @@ package LeetCodeSolution.AlgorithmThought._06_Search._79_WordSearch;
  *
  * 思路：
  * 1. 直接使用回溯法；
- * 2. 但注意边界条件、最终的目标设置以及约束条件。
+ * 2. 但注意边界条件、最终的目标设置以及约束条件；
+ * 3. 需要注意的是，其实题目并没有要求必须从 (0, 0) 开始找到目标单词，而是没有设定起始位置；
+ * 4. 如果必须从 (0, 0) 开始找目标单词，那么需要在 exist 函数的 for 中直接返回 dfs()，而不是再做判断。
  */
 public class Solution {
-    public static boolean exist(char[][] board, String word) {
+    public boolean exist(char[][] board, String word) {
         if (board == null || board.length == 0 || board[0].length == 0
-            || word == null || word.length() == 0) {
+                || word == null || word.length() == 0) {
             return false;
         }
 
+        int m = board.length, n = board[0].length;
         char[] ch = word.toCharArray();
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, i, j, ch, 0)) {
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (dfs(board, row, col, ch, 0)) {
                     return true;
                 }
             }
         }
+
         return false;
     }
 
-    public static boolean dfs(char[][] board, int i, int j, char[] ch, int index) {
-        // check the boundary
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != ch[index]) {
+    private boolean dfs(char[][] board, int row, int col, char[] ch, int index) {
+        int m = board.length, n = board[0].length;
+        // 边界检查
+        if (row < 0 || row >= m || col < 0 || col >= n || board[row][col] != ch[index]) {
             return false;
         }
 
@@ -41,15 +46,15 @@ public class Solution {
             return true;
         }
 
-        char temp = board[i][j];
-        board[i][j] = '#';
-        boolean res = dfs(board, i + 1, j, ch, index + 1)
-                || dfs(board, i - 1, j, ch, index + 1)
-                || dfs(board, i, j + 1, ch, index + 1)
-                || dfs(board, i, j - 1, ch, index + 1);
+        char temp = board[row][col];
+        board[row][col] = '#';
+        boolean res = dfs(board, row + 1, col, ch, index + 1)
+                || dfs(board, row - 1, col, ch, index + 1)
+                || dfs(board, row, col + 1, ch, index + 1)
+                || dfs(board, row, col - 1, ch, index + 1);
         // 由于涉及到回溯操作，因此需要再次将 [i][j] 位置设置为之前的字符
         // 再从之前的字符开始找另一条路径
-        board[i][j] = temp;
+        board[row][col] = temp;
         return res;
     }
 
@@ -62,8 +67,10 @@ public class Solution {
         String word1 = "ABCCED";
         String word2 = "SEE";
         String word3 = "ABCB";
-        System.out.println(exist(board, word1)); // true
-        System.out.println(exist(board, word2)); // true
-        System.out.println(exist(board, word3)); // false
+
+        Solution solution = new Solution();
+        System.out.println(solution.exist(board, word1)); // true
+        System.out.println(solution.exist(board, word2)); // true
+        System.out.println(solution.exist(board, word3)); // false
     }
 }
