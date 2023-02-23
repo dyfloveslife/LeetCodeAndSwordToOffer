8d9561fe9aceca5f4cbd54715158f00317dad31e
dyfloveslife <786616590@qq.com>
2023-02-23 14:23:13 +0800

    feat: update the code for question 164.

--- src/Other/BasicAlgorithm/_12_BucketSort/Solution.java
+++ src/Other/BasicAlgorithm/_12_BucketSort/Solution.java
@@ -20,55 +20,71 @@ package Other.BasicAlgorithm._12_BucketSort;
  *  7. 从头遍历桶，如果该桶为空，则继续遍历下一个桶；如果是非空桶，则找前一个离它最近的非空桶。
  *  8. 计算 前一个离它最近的非空桶的最大值 和 当前非空桶的最小值之间的 差值，即用最小值减最大值。
  *  9. 就这样可以算出许多的差值，然后用一个全局变量，计算出这些差值中的最大值即可。
- *
  */
 public class Solution {
-    public static int maxGap(int[] nums) {
+    public int maxGap(int[] nums) {
         if (nums == null || nums.length < 2) {
             return 0;
         }
 
-        int len = nums.length;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        int n = nums.length;
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
         // 首先找到数组中的最小值和最大值
-        for (int i = 0; i < len; i++) {
-            min = Math.min(min, nums[i]);
-            max = Math.max(max, nums[i]);
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
         // 说明数组中只有一种数，则返回 0
         if (min == max) {
             return 0;
         }
 
-        // 定义三个数组，表示每个桶中的信息，例如桶中是否有数、桶中的最大值、桶中的最小值
-        boolean[] hasNum = new boolean[len + 1];
-        int[] maxs = new int[len + 1];
-        int[] mins = new int[len + 1];
-        int bucketId = 0;
+        // 定义三个数组，表示每个桶中的信息，分别表示桶中是否有数、桶中的最大值、桶中的最小值
+        boolean[] hasNum = new boolean[n + 1];
+        int[] maxs = new int[n + 1];
+        int[] mins = new int[n + 1];
+        int bucketId;
         // 当前数字去几号桶，相应的桶内的信息（最大值、最小值、桶内是否有数字）就得更新
-        for (int i = 0; i < len; i++) {
-            bucketId = bucket(nums[i], len, min, max);
-            mins[bucketId] = hasNum[bucketId] ? Math.min(mins[bucketId], nums[i]) : nums[i];
-            maxs[bucketId] = hasNum[bucketId] ? Math.max(maxs[bucketId], nums[i]) : nums[i];
+        for (int num : nums) {
+            bucketId = getBucketId(n, num, min, max);
+            mins[bucketId] = hasNum[bucketId] ? Math.min(mins[bucketId], num) : num;
+            maxs[bucketId] = hasNum[bucketId] ? Math.max(maxs[bucketId], num) : num;
             hasNum[bucketId] = true;
         }
 
-        int res = 0;
+        int ans = 0;
         int lastMax = maxs[0];
-        int i = 1;
         // 找到每一个非空桶和离它左边最近的非空桶，用当前的最小减左边的最大即可
-        for (; i <= len; i++) {
+        for (int i = 1; i <= n; i++) {
             if (hasNum[i]) {
-                res = Math.max(res, mins[i] - lastMax);
+                ans = Math.max(ans, mins[i] - lastMax);
+                // lastMax 用于记录"左边最近的非空桶的最大值"
                 lastMax = maxs[i];
             }
         }
-        return res;
+
+        return ans;
     }
 
     // 当前数字进对应的桶
-    public static int bucket(long num, long len, long min, long max) {
-        return (int) ((num - min) * len / (max - min));
+    private int getBucketId(long n, long num, long min, long max) {
+        return (int) ((num - min) * n / (max - min));
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {3, 6, 9, 1};
+        int[] nums2 = {10};
+        int[] nums3 = {15255, 15256, 15257};
+        int[] nums4 = {100, 3, 2, 1};
+        int[] nums5 = {1, 1, 1, 1};
+        int[] nums6 = {2, 99999999};
+
+        System.out.println(solution.maxGap(nums1)); // 3
+        System.out.println(solution.maxGap(nums2)); // 0
+        System.out.println(solution.maxGap(nums3)); // 1
+        System.out.println(solution.maxGap(nums4)); // 97
+        System.out.println(solution.maxGap(nums5)); // 0
+        System.out.println(solution.maxGap(nums6)); // 99999997
     }
 }
