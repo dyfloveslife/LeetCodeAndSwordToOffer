99718d0b9e8554ad04d629c547eb810662eec625
dyfloveslife <786616590@qq.com>
2023-02-24 11:19:35 +0800

    feat: update the code for question 217.

--- src/LeetCodeSolution/DataStructure/_04_HashTable/_217_ContainsDuplicate/Solution.java
+++ src/LeetCodeSolution/DataStructure/_04_HashTable/_217_ContainsDuplicate/Solution.java
@@ -1,6 +1,8 @@
 package LeetCodeSolution.DataStructure._04_HashTable._217_ContainsDuplicate;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 /*
  * 存在重复元素
@@ -11,10 +13,11 @@ import java.util.HashSet;
  * 如果数组中每个元素都不相同，则返回 false。
  *
  * 思路：
- * 1. 使用 HashSet 解决。
+ * 1. 使用 HashSet 判断是否重复；
+ * 2. 使用 HashMap 计数。
  */
 public class Solution {
-    public boolean containsDuplicate(int[] nums) {
+    public boolean containsDuplicate1(int[] nums) {
         if (nums == null || nums.length == 0) {
             return false;
         }
@@ -27,6 +30,23 @@ public class Solution {
                 set.add(num);
             }
         }
+
+        return false;
+    }
+
+    public boolean containsDuplicate2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.getOrDefault(num, 0) > 1) {
+                return true;
+            }
+        }
+
         return false;
     }
 
@@ -34,8 +54,18 @@ public class Solution {
         Solution solution = new Solution();
         int[] nums1 = {1, 2, 3, 1};
         int[] nums2 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        int[] nums3 = {1, 2, 3, 4};
+        int[] nums4 = {3, 3};
 
-        System.out.println(solution.containsDuplicate(nums1));
-        System.out.println(solution.containsDuplicate(nums2));
+        System.out.println(solution.containsDuplicate1(nums1)); // true
+        System.out.println(solution.containsDuplicate1(nums2)); // true
+        System.out.println(solution.containsDuplicate1(nums3)); // false
+        System.out.println(solution.containsDuplicate1(nums4)); // true
+
+        System.out.println("---");
+        System.out.println(solution.containsDuplicate2(nums1)); // true
+        System.out.println(solution.containsDuplicate2(nums2)); // true
+        System.out.println(solution.containsDuplicate2(nums3)); // false
+        System.out.println(solution.containsDuplicate2(nums4)); // true
     }
 }
