2bbf0acdedd114d17f86814074282a1bab8eb782
dyfloveslife <786616590@qq.com>
2023-02-28 15:37:16 +0800

    feat: update the code for question 253.

--- src/LeetCodeSolution/DataStructure/_01_ArrayAndMatrix/_253_MeetingRoomsII/Solution.java
+++ src/LeetCodeSolution/DataStructure/_01_ArrayAndMatrix/_253_MeetingRoomsII/Solution.java
@@ -2,6 +2,7 @@ package LeetCodeSolution.DataStructure._01_ArrayAndMatrix._253_MeetingRoomsII;
 
 import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 /*
  * 会议室 Ⅱ
@@ -13,10 +14,10 @@ import java.util.PriorityQueue;
  * 思路：
  * 1. 使用小顶堆；
  * 2. 首先按照每个会议的开始时间进行排序；
- * 3. 创建一个小顶堆，堆顶元素填入第一个会议的结束时间；
- * 4. 以此遍历每个会议，检查堆顶的会议室是否空闲，也就是检查每个会议开始的时间是否大于堆顶会议结束的时间：
- *    如果空闲，则弹出堆顶元素，然后加入新的会议；
- *    如果不空闲，则新开一个会议室，也就是加入新的会议室；
+ * 3. 创建一个小顶堆，堆顶元素填入第一个会议的结束时间；（堆中存储的是会议室的结束时间）
+ * 4. 依次遍历每个会议，检查堆顶的会议室是否空闲，也就是检查当前遍历到的会议的开始时间是否大于等于堆顶会议的结束时间：
+ *    如果空闲，说明不能充分利用会议室资源，则弹出堆顶元素，并加入当前会议；
+ *    如果不空闲，即当前遍历到的会议的开始时间小于堆顶会议的结束时间，则说明可以充分这两个会议室的资源，然后将当前会议室的结束时间加入到堆中；
  * 5. 最后返回堆的个数，就是会议室的数量。
  */
 public class Solution {
@@ -24,8 +25,9 @@ public class Solution {
         if (intervals == null || intervals.length == 0 || intervals[0].length == 0) {
             return 0;
         }
+
         Arrays.sort(intervals, (o1, o2) -> (o1[0] - o2[0]));
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        Queue<Integer> minHeap = new PriorityQueue<>();
         minHeap.offer(intervals[0][1]);
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] >= minHeap.peek()) {
@@ -33,15 +35,16 @@ public class Solution {
             }
             minHeap.offer(intervals[i][1]);
         }
+
         return minHeap.size();
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[][] intervals = {{0, 30}, {5, 10}, {15, 20}};
-        int[][] intervals1 = {{13, 15}, {1, 13}};
+        int[][] intervals1 = {{0, 30}, {5, 10}, {15, 20}};
+        int[][] intervals2 = {{13, 15}, {1, 13}};
 
-        System.out.println(solution.minMeetingRooms(intervals));
-        System.out.println(solution.minMeetingRooms(intervals1));
+        System.out.println(solution.minMeetingRooms(intervals1)); // 2
+        System.out.println(solution.minMeetingRooms(intervals2)); // 1
     }
 }
