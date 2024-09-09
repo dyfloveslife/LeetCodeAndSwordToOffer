3cd1159eb8995581062a94d645a7da8391af9e9d
dyfloveslife <786616590@qq.com>
2024-09-09 18:14:47 +0800

    feat(*): append `merge sort` code

--- src/Other/BasicAlgorithm/_06_MergeSort/Solution.java
+++ src/Other/BasicAlgorithm/_06_MergeSort/Solution.java
@@ -2,9 +2,9 @@ package Other.BasicAlgorithm._06_MergeSort;
 
 import java.util.Arrays;
 
-/*
+/**
  * 归并排序
- *
+ * <p>
  * 原理：
  * 0. 将已有序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再使子序列段间有序。若将两个有序表合并成一个有序表，称为2-路归并。
  * 1. 先将数组分成两部分，然后分别将这两部分排好序；
@@ -12,7 +12,7 @@ import java.util.Arrays;
  * 3. 比较 a 和 b 的大小，小的放进数组中。如果某个指针到了末尾的话，则另一个指针所指元素的
  *    后面所有的数直接进数组。
  * 4. 最后再将排好序的顺序拷贝到原数组中。
- *
+ * <p>
  * 时间复杂度：O(N*logN)
  * 空间复杂度：O(N)
  * 稳定
@@ -27,7 +27,7 @@ public class Solution {
         return nums;
     }
 
-    private static void sortProcess(int[] nums, int left, int right) {
+    private void sortProcess(int[] nums, int left, int right) {
         // 当前范围只有一个数，则本身就已经排好序了
         if (left == right) {
             return;
@@ -40,9 +40,9 @@ public class Solution {
     }
 
     // 左部分和右部分都已经排好序了，现在需要整体合并再排序
-    private static void merge(int[] nums, int left, int mid, int right) {
+    private void merge(int[] nums, int left, int mid, int right) {
         int[] help = new int[right - left + 1];
-        
+
         // i 的作用就是在谁小填谁的时候，从 0 位置开始填
         int idx = 0;
         // p1 和 p2 分别指向两个子数组的起始元素
---
+++ src/Other/BasicAlgorithm/_06_MergeSort/Solution2.java
@@ -0,0 +1,108 @@
+package Other.BasicAlgorithm._06_MergeSort;
+
+import java.util.Arrays;
+
+/**
+ * Merge sort.
+ *
+ * @author Youfang Duan
+ * @since 2024/09/09 17:57
+ */
+public class Solution2 {
+
+    /**
+     * Merge sort.
+     *
+     * @param nums int[]
+     * @return int[]
+     */
+    public int[] mergeSort(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return new int[0];
+        }
+
+        sortProcess(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    /**
+     * Sort process.
+     *
+     * @param nums  int[]
+     * @param left  int
+     * @param right int
+     */
+    private void sortProcess(int[] nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int mid = left + ((right - left) >> 1);
+        sortProcess(nums, left, mid);
+        sortProcess(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
+
+    /**
+     * Merge element.
+     *
+     * @param nums  int[]
+     * @param left  int
+     * @param mid   int
+     * @param right right
+     */
+    private void merge(int[] nums, int left, int mid, int right) {
+        // 计算两个子数组的大小
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        // 将数据放到临时数组
+        for (int i = 0; i < n1; i++) {
+            leftArr[i] = nums[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            rightArr[j] = nums[mid + 1 + j];
+        }
+
+        // 合并临时数组
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] < rightArr[j]) {
+                nums[k] = leftArr[i];
+                i++;
+            } else {
+                nums[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // 复制剩余元素
+        while (i < n1) {
+            nums[k] = leftArr[i];
+            k++;
+            i++;
+        }
+
+        while (j < n2) {
+            nums[k] = rightArr[j];
+            k++;
+            j++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {3, 5, 1, 7, -10, 4, 6, 12, 1};
+        int[] nums2 = {5, 2, 3, 1};
+        int[] nums3 = {5, 1, 1, 2, 0, 0};
+
+        Solution2 solution = new Solution2();
+        System.out.println(Arrays.toString(solution.mergeSort(nums1)));
+        System.out.println(Arrays.toString(solution.mergeSort(nums2)));
+        System.out.println(Arrays.toString(solution.mergeSort(nums3)));
+    }
+}
